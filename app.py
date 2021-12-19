074af3200a7fc2ab110f7767557549d4a256c22c
yc <yungchunchen0618@gmail.com>
2021-12-19 18:23:48 +0800

    make it better

--- app.py
+++ app.py
@@ -1,3 +1,142 @@
+# import os
+# import sys
+
+# from flask import Flask, jsonify, request, abort, send_file
+# from dotenv import load_dotenv
+# from linebot import LineBotApi, WebhookParser
+# from linebot.exceptions import InvalidSignatureError
+# from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# from fsm import TocMachine
+# from utils import send_text_message
+
+# load_dotenv()
+
+
+
+# machine = TocMachine(
+#     states=["user", "state1", "state2", "state3"],
+#     transitions=[
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "state1",
+#             "conditions": "is_going_to_state1",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "state2",
+#             "conditions": "is_going_to_state2",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "state3",
+#             "conditions": "is_going_to_state3",
+#         },
+#         {"trigger": "go_back", "source": ["state1", "state2", "state3"], "dest": "user"},
+#     ],
+#     initial="user",
+#     auto_transitions=False,
+#     show_conditions=True,
+# )
+
+# app = Flask(__name__, static_url_path="")
+
+
+# # get channel_secret and channel_access_token from your environment variable
+# channel_secret = "7bd4cc4ffdbdd3870f2596ae9ef88c9b"
+# channel_access_token = "lP+b/BM0OXIegi4iJkTIDSTkaLObmNnzcOa6Q6WKhVD/brx47xAdEIIKSjebM6kT7HugaVykdrQWgjQcAwYXVoZqZN3T0dRrSAhdc5cg0qW9v8W/5gJR4K3b98xRL1in0ebUo2XM/5ibtHCi+d/ETQdB04t89/1O/w1cDnyilFU="
+# if channel_secret is None:
+#     print("Specify LINE_CHANNEL_SECRET as environment variable.")
+#     sys.exit(1)
+# if channel_access_token is None:
+#     print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+#     sys.exit(1)
+
+# line_bot_api = LineBotApi(channel_access_token)
+# parser = WebhookParser(channel_secret)
+
+
+# @app.route("/callback", methods=["POST"])
+# def callback():
+#     signature = request.headers["X-Line-Signature"]
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+
+#     # parse webhook body
+#     try:
+#         events = parser.parse(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+
+#     # if event is MessageEvent and message is TextMessage, then echo text
+#     for event in events:
+#         if not isinstance(event, MessageEvent):
+#             continue
+#         if not isinstance(event.message, TextMessage):
+#             continue
+
+#         line_bot_api.reply_message(
+#             event.reply_token, TextSendMessage(text=event.message.text)
+#         )
+
+#     return "OK"
+
+
+# @app.route("/webhook", methods=["POST"])
+# def webhook_handler():
+#     signature = request.headers["X-Line-Signature"]
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info(f"Request body: {body}")
+
+#     # parse webhook body
+#     try:
+#         events = parser.parse(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+
+#     # if event is MessageEvent and message is TextMessage, then echo text
+#     for event in events:
+#         if not isinstance(event, MessageEvent):
+#             continue
+#         if not isinstance(event.message, TextMessage):
+#             continue
+#         if not isinstance(event.message.text, str):
+#             continue
+#         print(f"\nFSM STATE: {machine.state}")
+#         print(f"REQUEST BODY: \n{body}")
+#         response = machine.advance(event)
+#         if response == False:
+#             send_text_message(event.reply_token, "Not Entering any State")
+
+#     return "OK"
+
+
+# @app.route("/show-fsm", methods=["GET"])
+# def show_fsm():
+#     machine.get_graph().draw("fsm.png", prog="dot", format="png")
+#     return send_file("fsm.png", mimetype="image/png")
+
+
+# if __name__ == "__main__":
+#     port = os.environ.get("PORT", 8000)
+#     app.run(host="0.0.0.0", port=port, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 import sys
 
@@ -5,37 +144,68 @@ from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 from fsm import TocMachine
-from utils import send_text_message
+from utils import send_text_message, add_food_message
+from random import choice
 
 load_dotenv()
 
-
+food = ['鴨肉飯', '乾麵', '港式燒臘', '鍋燒意麵', '炒飯', '拉麵', '餛飩麵']
 
 machine = TocMachine(
-    states=["user", "state1", "state2", "state3"],
+    states=["user", "choosefood", "all_food", "add_food", "delete_food", "show_foodphoto", "no_food", "have_food"],
     transitions=[
         {
             "trigger": "advance",
             "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
+            "dest": "choosefood",
+            "conditions": "is_going_to_choosefood",
         },
         {
             "trigger": "advance",
             "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
+            "dest": "all_food",
+            "conditions": "is_going_to_all_food",
         },
         {
             "trigger": "advance",
             "source": "user",
-            "dest": "state3",
-            "conditions": "is_going_to_state3",
+            "dest": "add_food",
+            "conditions": "is_adding_food",
         },
-        {"trigger": "go_back", "source": ["state1", "state2", "state3"], "dest": "user"},
+        {
+            "trigger": "advance",
+            "source": "add_food",
+            "dest": "user",
+            "conditions": "not_empty",
+        },
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "delete_food",
+            "conditions": "is_deleting_food",
+        },
+        {
+            "trigger": "advance",
+            "source": "delete_food",
+            "dest": "have_food",
+            "conditions": "food_is_in_list",
+        },
+        {
+            "trigger": "advance",
+            "source": "delete_food",
+            "dest": "no_food",
+            "conditions": "food_isnot_in_list",
+        },
+        {
+            "trigger": "advance",
+            "source": "user",
+            "dest": "show_foodphoto",
+            "conditions": "is_showing_foodphoto",
+        },
+        {"trigger": "go_back", "source": ["choosefood", "all_food", "show_foodphoto", "no_food", "have_food"], "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
@@ -79,9 +249,17 @@ def callback():
         if not isinstance(event.message, TextMessage):
             continue
 
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
+        if event.message.text == "吃什麼":
+            line_bot_api.reply_message(event.reply_token, TextMessage(text = choice(food)))
+
+        elif event.message.text == "圖片":
+            image_url = 'https://images.zi.org.tw/ireneslife/2018/08/23222608/1535034368-95cf834c4d3687e1347ed20f3cdb7cab.jpg'
+            line_bot_api.reply_message(event.reply_token, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
+        
+        else:
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text=event.message.text)
+            )
 
     return "OK"
 
@@ -111,8 +289,7 @@ def webhook_handler():
         print(f"REQUEST BODY: \n{body}")
         response = machine.advance(event)
         if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
-
+            send_text_message(event.reply_token, "請輸入「吃什麼」決定下一餐\n請輸入「有什麼」看現有食物種類\n請輸入「加食物」新增想要的食物\n請輸入「刪食物」刪除不要的食物\n請輸入「照片」顯示美食照片")
     return "OK"
 
 
--- fsm.py
+++ fsm.py
@@ -1,53 +1,163 @@
+# from transitions.extensions import GraphMachine
+
+# from utils import send_text_message
+
+
+# class TocMachine(GraphMachine):
+#     def __init__(self, **machine_configs):
+#         self.machine = GraphMachine(model=self, **machine_configs)
+
+#     def is_going_to_state1(self, event):
+#         text = event.message.text
+#         return text.lower() == "go to state1"
+
+#     def is_going_to_state2(self, event):
+#         text = event.message.text
+#         return text.lower() == "go to state2"
+
+#     def is_going_to_state3(self, event):
+#         text = event.message.text
+#         return text.lower() == "go to state3"
+
+# #
+#     def on_enter_state1(self, event):
+#         print("I'm entering state1")
+
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Trigger state1")
+#         self.go_back()
+
+#     def on_exit_state1(self):
+#         print("Leaving state1")
+
+# #
+#     def on_enter_state2(self, event):
+#         print("I'm entering state2")
+
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Trigger state2")
+#         self.go_back()
+
+#     def on_exit_state2(self):
+#         print("Leaving state2")
+
+# #
+#     def on_enter_state3(self, event):
+#         print("I'm entering state3")
+
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Trigger state3")
+#         self.go_back()
+
+#     def on_exit_state3(self):
+#         print("Leaving state3")
+
+
+
+
+
+
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
-
+from utils import send_text_message, send_image_url, send_food_message, add_food_message, send_allfood_message, delete_food_message, is_food
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_choosefood(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text.lower() == "吃什麼"
 
-    def is_going_to_state2(self, event):
+    def is_going_to_all_food(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "有什麼"
 
-    def is_going_to_state3(self, event):
+    def is_adding_food(self,event):
         text = event.message.text
-        return text.lower() == "go to state3"
+        return text.lower() == "加食物"
 
-#
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    def is_deleting_food(self, event):
+        text = event.message.text
+        return text.lower() == "刪食物"
 
+    def not_empty(self, event):
+        text = event.message.text
+        return (text.lower() != "" and not(is_food(text.lower())))
+
+    def food_is_in_list(self, event):
+        text = event.message.text
+        return is_food(text.lower())
+
+    def food_isnot_in_list(self, event):
+        text = event.message.text
+        return not(is_food(text.lower()))
+
+    def is_showing_foodphoto(self, event):
+        text = event.message.text
+        return text.lower() == "照片"
+
+    def on_enter_choosefood(self, event):
+        print("choose one food")
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        send_food_message(reply_token)
         self.go_back()
 
-    def on_exit_state1(self):
+    def on_exit_choosefood(self):
         print("Leaving state1")
 
-#
-    def on_enter_state2(self, event):
+    def on_enter_all_food(self, event):
         print("I'm entering state2")
-
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        send_allfood_message(reply_token)
         self.go_back()
 
-    def on_exit_state2(self):
+    def on_exit_all_food(self):
         print("Leaving state2")
-
-#
-    def on_enter_state3(self, event):
-        print("I'm entering state3")
-
+        
+    def on_enter_add_food(self, event):
+        print("I'm adding food")
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state3")
+        send_text_message(reply_token, "請輸入要新增的食物")
+
+    def on_exit_add_food(self, event):
+        print("Leaving add_food")
+        reply_token = event.reply_token
+        add_food_message(event.message.text.lower())
+        send_text_message(reply_token, "已新增")
+
+    def on_enter_delete_food(self, event):
+        print("I'm deleting food")
+        reply_token = event.reply_token
+        send_text_message(reply_token, "請輸入要刪除的食物")
+
+    def on_exit_delete_food(self, event):
+        print("Leaving delete_food")
+
+    def on_enter_have_food(self, event):
+        print("food deleted")
+        reply_token = event.reply_token
+        delete_food_message(event.message.text.lower())
+        send_text_message(reply_token, "已刪除")
         self.go_back()
 
-    def on_exit_state3(self):
-        print("Leaving state3")
+    def on_exit_have_food(self, event):
+        print("Leaving have_food")
+
+    def on_enter_show_foodphoto(self, event):
+        print("show food photo")
+        reply_token = event.reply_token
+        send_image_url(reply_token)
+        self.go_back()
+
+    def on_exit_show_foodphoto(self):
+        print("Leaving show_foodphoto")
+
+    def on_enter_no_food(self, event):
+        print("I'm entering no_food")
+        reply_token = event.reply_token
+        send_text_message(reply_token, "清單中沒有該食物")
+        self.go_back()
+
+    def on_exit_no_food(self):
+        print("Leaving no_food")
